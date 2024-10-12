7e1a09017da859e41e56a397d41787c752fec23a
Gerry <gerryfranchetti@gmail.com>
2024-10-12 15:54:11 +0100

    added hug

---
+++ legacy/luascripts/wolfadmin/commands/admin/hbchug.lua
@@ -0,0 +1,57 @@
+
+-- WolfAdmin module for Wolfenstein: Enemy Territory servers.
+-- Copyright (C) 2015-2017 Timo 'Timothy' Smit
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- at your option any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+local auth = wolfa_requireModule("auth.auth")
+
+local commands = wolfa_requireModule("commands.commands")
+
+local players = wolfa_requireModule("players.players")
+
+local settings = wolfa_requireModule("util.settings")
+
+function commandHug(clientId, command, victim)
+    local cmdClient
+
+    if victim == nil then
+        et.trap_SendConsoleCommand(et.EXEC_APPEND, "csay "..clientId.." \"^dhug usage: "..commands.getadmin("hug")["syntax"].."\";")
+
+        return true
+    elseif tonumber(victim) == nil or tonumber(victim) < 0 or tonumber(victim) > tonumber(et.trap_Cvar_Get("sv_maxclients")) then
+        cmdClient = et.ClientNumberFromString(victim)
+    else
+        cmdClient = tonumber(victim)
+    end
+
+    if cmdClient == -1 or cmdClient == nil then
+        et.trap_SendConsoleCommand(et.EXEC_APPEND, "csay "..clientId.." \"^dhug: ^9no or multiple matches for '^7"..victim.."^9'.\";")
+
+        return true
+    elseif not et.gentity_get(cmdClient, "pers.netname") then
+        et.trap_SendConsoleCommand(et.EXEC_APPEND, "csay "..clientId.." \"^dhug: ^9no connected player by that name or slot #\";")
+
+        return true
+    end
+
+    local oldName = players.getName(cmdClient)
+
+    local clientInfo = et.trap_GetUserinfo(cmdClient)
+
+    et.trap_SendConsoleCommand(et.EXEC_APPEND, "cchat -1 \"."..players.getName(clientId).."^dhugs ^7"..oldName.."^9 and scores 0 XP.^9.\";")
+
+    return true
+end
+commands.addadmin("hug", commandHug, auth.PERM_HUG, "hugs a player", nil, (settings.get("g_standalone") == 0))
